ebad5920f7dff30db05e012e64e54c5784d9e109
Benjamin Odell <benjamin.m.odell@proton.me>
2026-04-21 11:01:55 -0400

    Create Index view and started work on user auth

--- FinalProject/accounts/views.py
+++ FinalProject/accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def create_account(request):
+    error = None
+    if request.method == "POST":
+        #create user
+
+        #check for username and password
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        if username is None:
+            error = "Username is required."
+        elif password is None:
+            error = "Password is required."
+        else:
+            #check for user
+            user = User.objects.filter(username=username)
+            if user:
+                error = "Username already exists."
+            else:
+                User.objects.create_user(username=username, password=password)
+                return HttpResponseRedirect(reverse("login"))
+
+    #render page
+    return render(request, 'accounts/create_user.html', {"error": error})
+
+def login(request):
+    pass
