d839f20db138d01edd3f4bfdcff6ffeb80dddae9
Benjamin Odell <benjamin.m.odell@proton.me>
2026-04-22 00:25:28 -0400

    Added models for the space app

--- FinalProject/space/models.py
+++ FinalProject/space/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    date = models.DateField()
+    likes = models.IntegerField(default=0)
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.DateTimeField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.DateTimeField()
+    comment = models.TextField()
+
+
+
+
